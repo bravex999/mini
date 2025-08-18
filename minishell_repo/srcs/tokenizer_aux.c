6ea13386e575848b597e29d02023d88397d2a1a0
naranjox <cdny195@gmail.com>
2025-08-18 15:55:09 +0200

    jh

--- minishell_repo/srcs/tokenizer_aux.c
+++ minishell_repo/srcs/tokenizer_aux.c
@@ -1,78 +1,70 @@
 #include "minishell.h"
 
-t_token *handle_quotes(char *line, int *i, t_shell *shell)
+t_token	*handle_quotes(char *line, int *i, t_shell *shell)
 {
-    (void)shell;
-	t_token *line_token;
+	t_token	*line_token;
 
+	(void)shell;
 	if (line[*i] == '"')
-      line_token = handle_double(line, i, shell); 
-    else
+		line_token = handle_double(line, i, shell);
+	else
 		line_token = handle_single(line, i);
-	return(line_token);	
+	return (line_token);
 }
 
-t_token  *redir_operators_app_her(char *line, int *i)
+t_token	*redir_operators_app_her(char *line, int *i)
 {
-	char *line_token;
-	t_token *new_token;
-	new_token = NULL;
+	char	*line_token;
+	t_token	*new_token;
 
+	new_token = NULL;
 	line_token = ft_substr(line, *i, 2);
-	if(!line_token)
+	if (!line_token)
 		return (NULL);
-    if (line[*i + 1] == '>')
+	if (line[*i + 1] == '>')
 		new_token = create_token_safe(T_REDIR_APPEND, line_token);
-	else if(line[*i + 1] == '<')
+	else if (line[*i + 1] == '<')
 		new_token = create_token_safe(T_HEREDOC, line_token);
 	*i += 2;
-	return(new_token);
+	return (new_token);
 }
 
-t_token *simple_redir(char *line, int *i)
+t_token	*simple_redir(char *line, int *i)
 {
-	t_token *new_token;
-	char *line_token;
+	t_token	*new_token;
+	char	*line_token;
+
 	new_token = NULL;
-	
 	line_token = ft_substr(line, *i, 1);
-	if(!line_token)
+	if (!line_token)
 		return (NULL);
 	if (line[*i] == '|')
 		new_token = create_token_safe(T_PIPE, line_token);
 	else if (line[*i] == '<')
 		new_token = create_token_safe(T_REDIR_IN, line_token);
-	else if(line[*i] == '>')
+	else if (line[*i] == '>')
 		new_token = create_token_safe(T_REDIR_OUT, line_token);
 	(*i)++;
-	return(new_token);
+	return (new_token);
 }
 
-t_token *words(char *line, int *i, t_shell *shell)
+t_token	*words(char *line, int *i, t_shell *shell)
 {
-    (void)shell;
-	int start_word;
-    char *line_token;
-    int len;
-  /*char *expand_line;*/
-  
-     len = 0;
-     start_word = *i;
-     while(line[*i] && line[*i] != ' ' && line[*i] != '"' && line[*i] != '\'' &&
-                   line[*i] != '|' && line[*i] != '>' && line[*i] != '<')
-     {
-         len++;
-         (*i)++;
-     }
-     line_token = ft_substr(line, start_word, len);
-     if(!line_token)
-         return (NULL);
-    /* if(ft_strchr(line_token, '$'))
-     {
-         expand_line = expander(line_token, shell);
-         free(line_token);
-         return(expand_split(expand_line));
-     }*/   
-     return(create_token_safe(T_WORD, line_token));
-}
+	int		start_word;
+	char	*line_token;
+	int		len;
 
+	(void)shell;
+	len = 0;
+	start_word = *i;
+	while (line[*i] && line[*i] != ' ' && line[*i] != '"' && line[*i] != '\''
+		&& line[*i] != '|' && line[*i] != '>' && line[*i] != '<')
+	{
+		len++;
+		(*i)++;
+	}
+	line_token = ft_substr(line, start_word, len);
+	if (!line_token)
+		return (NULL);
+	return (create_token_safe(T_WORD, line_token));
+}
