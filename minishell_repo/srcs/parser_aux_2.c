1b08f712ddddc6aa87dbb1a1932820fc33559bf1
naranjox <cdny195@gmail.com>
2025-08-18 16:25:21 +0200

    avance

--- minishell_repo/srcs/parser_aux_2.c
+++ minishell_repo/srcs/parser_aux_2.c
@@ -1,17 +1,17 @@
 #include "minishell.h"
 
-int has_redirections(t_token *token_list)
+int	has_redirections(t_token *token_list)
 {
-    t_token *current;
+	t_token	*current;
 
-    current = token_list;
-    while (current)
-    {
-        if (current->type == T_REDIR_IN ||
-            current->type == T_REDIR_OUT ||
-            current->type == T_REDIR_APPEND)
-            return (1);
-        current = current->next;
-    }
-    return (0);
+	current = token_list;
+	while (current)
+	{
+		if (current->type == T_REDIR_IN
+			|| current->type == T_REDIR_OUT
+			|| current->type == T_REDIR_APPEND)
+			return (1);
+		current = current->next;
+	}
+	return (0);
 }
