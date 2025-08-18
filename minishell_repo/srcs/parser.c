663cba84a08eb35174a49df035d23cd187aa38e1
naranjox <cdny195@gmail.com>
2025-08-18 16:05:01 +0200

    j

--- minishell_repo/srcs/parser.c
+++ minishell_repo/srcs/parser.c
@@ -1,68 +1,49 @@
 #include "minishell.h" 
 
-static int general_check(t_token *token_list);
-static t_cmd *build_commands(t_token *token_listi, t_shell *shell);
+static int		general_check(t_token *token_list);
+static t_cmd	*build_commands(t_token *token_listi, t_shell *shell);
 
-t_cmd   *parse_tokens(t_token *token_list, t_shell *shell)
+t_cmd	*parse_tokens(t_token *token_list, t_shell *shell)
 {
-	(void)shell;
-	t_token *current;
-	current = token_list;
+	t_token	*current;
 
-	if(current)
+	current = token_list;
+	(void)shell;
+	if (current)
 	{
-		if(general_check(token_list))
+		if (general_check(token_list))
 		{	
-			printf("no se genero token\n");
 			return (NULL);
 		}
 		return (build_commands(token_list, shell));
 	}
-	return(NULL);
+	return (NULL);
 }
 
-static int general_check(t_token *token_list)
+static int	general_check(t_token *token_list)
 {
 	if (check_initial_token(token_list))
-		return(1);
+		return (1);
 	if (check_all_redirections(token_list))
-		return(1);
-	if (check_all_pipe(token_list))	
-		return(1);
-	return(0);
+		return (1);
+	if (check_all_pipe(token_list))
+		return (1);
+	return (0);
 }
 
-t_cmd *build_commands(t_token *token_list, t_shell *shell)
+t_cmd	*build_commands(t_token *token_list, t_shell *shell)
 {
-	t_cmd *new_command;
+	t_cmd	*new_command;
+
 	new_command = NULL;
-	
 	if (is_heredoc(token_list))
 		new_command = (build_heredoc_cmd(token_list, shell));
 	else if (has_redirections(token_list))
 		new_command = build_command_with_redirections(token_list);
-	else if(is_token_word(token_list) && token_list->next == NULL)
+	else if (is_token_word(token_list) && token_list->next == NULL)
 		new_command = one_word_command(token_list);
-	else if(is_token_word(token_list) && token_list->next != NULL)
+	else if (is_token_word(token_list) && token_list->next != NULL)
 		new_command = words_plus_cmd(token_list);
-	
-	return(new_command);
+	return (new_command);
+	return (NULL);
 }
-/*
-	printf("si se genero token\n");
-	print_token_list(token_list);
-	return(NULL);
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
